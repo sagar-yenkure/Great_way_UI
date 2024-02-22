d915ef82d451c67cd504b8e1867c3cf4988969bb
rathodrahulm <rahulrathod276@gmail.com>
2024-02-22 17:44:20 +0530

    Contact us updated and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,23 @@
-
-import './App.css'
-import AboutUs from './components/AboutUs';
-import ContactUs from './components/ContactUs';
-import Home from './components/Home';
-import Navbar from './components/Navbar'
-import {
-  Route,
-  Routes
-}
-  from "react-router-dom";
-
+import "./App.css";
+import AboutUs from "./components/AboutUs";
+import ContactUs from "./components/ContactUs/ContactUs";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
- 
-
   return (
     <>
-    <Navbar/>
-    <Routes>
-          <Route exact path='/' element={<Home />}></Route>
-          <Route exact path='/AboutUs' element={<AboutUs />}></Route>
-          <Route exact path='/ContactUs' element={<ContactUs />}></Route>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/AboutUs" element={<AboutUs />}></Route>
+        <Route exact path="/ContactUs" element={<ContactUs />}></Route>
       </Routes>
-    
-
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
