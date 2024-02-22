b029eb1a033d67934ae311f846ef32f43cb7d82f
raviw45 <ravikantwaghmare82@gmail.com>
2024-02-22 18:13:00 +0530

    Routes Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,22 @@
-
-
-import {
-  Route,
-  Routes
-} from "react-router-dom";
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
 import AboutUs from "./components/AboutUs/AboutUs";
 import CareersPage from "./components/Careers/CareersPage";
 import ContactUs from "./components/ContactUs/ContactUs";
 import Footer from "./components/Footer";
-import Home from './components/Home';
-import Navbar from './components/Navbar/Navbar';
-
+import Home from "./components/Home";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Routes>
-          <Route exact path='/' element={<Home />}></Route>
-          <Route exact path='/AboutUs' element={<AboutUs />}></Route>
-          <Route exact path='/ContactUs' element={<ContactUs />}></Route>
-          <Route exact path='/Career' element={<CareersPage/>}></Route>
-   
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/AboutUs" element={<AboutUs />}></Route>
+        <Route exact path="/ContactUs" element={<ContactUs />}></Route>
+        <Route exact path="/Career" element={<CareersPage />}></Route>
+       </Routes>
       <Footer />
     </>
   );
