4e3f63507f484d0f77d2e0ff837810181d298504
raviw45 <ravikantwaghmare82@gmail.com>
2024-02-26 18:00:57 +0530

    Modification about the technologies and contact us ui changes

--- src/components/Home/Tech.jsx
+++ src/components/Home/Tech.jsx
@@ -1,27 +1,20 @@
-import React from "react";
 import {
+  DiAndroid,
+  DiMysql,
+  DiWordpress
+} from "react-icons/di";
+import {
+  FaAngular,
+  FaAws,
+  FaCloud,
   FaJava,
   FaPython,
-  FaAngular,
-  FaReact,
-  FaMicrosoft,
-  FaSalesforce,
-  FaAws,
-  FaJira,
-  FaCloud,
-  FaDocker,
+  FaReact
 } from "react-icons/fa";
-import {
-  DiGit,
-  DiMysql,
-  DiLinux,
-  DiWordpress,
-  DiAndroid,
-} from "react-icons/di";
-import { SiSpringboot, SiHibernate, SiJest } from "react-icons/si";
-import { GrOracle, GrHadoop } from "react-icons/gr";
+import { GrHadoop, GrOracle } from "react-icons/gr";
 import { IoLogoJavascript } from "react-icons/io5";
 import { RiOpenaiFill } from "react-icons/ri";
+import { SiHibernate, SiSpringboot } from "react-icons/si";
 
 const Tech = () => {
   const Tech_list = [
@@ -31,22 +24,15 @@ const Tech = () => {
     { logo: GrOracle, name: "Oracle" },
     { logo: FaAngular, name: "Angular" },
     { logo: FaReact, name: "React" },
-    { logo: FaMicrosoft, name: "Microsoft" },
-    { logo: FaSalesforce, name: "Salesforce" },
     { logo: GrHadoop, name: "Hadoop" },
     { logo: SiSpringboot, name: "Spring Boot" },
     { logo: DiMysql, name: "MySQL" },
     { logo: SiHibernate, name: "Hibernate" },
-    { logo: DiGit, name: "Git" },
-    { logo: DiLinux, name: "Linux" },
     { logo: DiWordpress, name: "Wordpress" },
     { logo: DiAndroid, name: "Android" },
     { logo: FaAws, name: "AWS" },
-    { logo: FaJira, name: "Jira" },
     { logo: FaCloud, name: "Cloud" },
-    { logo: SiJest, name: "Jest" },
-    { logo: FaDocker, name: "Docker" },
-    { logo: RiOpenaiFill, name: "OpenAI" },
+    { logo: RiOpenaiFill, name: "ML and AI" },
   ];
 
   return (
@@ -54,13 +40,13 @@ const Tech = () => {
       <div className="flex p-8 justify-center text-white">
         <h1 className="text-4xl font-black">Technology's we working with</h1>
       </div>
-      <div className="Tech_list  flex justify-evenly flex-wrap gap-5 items-center ">
+      <div className="Tech_list p-10  flex justify-evenly flex-wrap gap-5 items-center ">
         {Tech_list.map((TechIcon) => (
           <div
             key={TechIcon.name}
-            className="w-[7rem] h-[7rem] hover:scale-110 hover:cursor-pointer transition-all ease-out bg-white flex-col flex rounded-full justify-center items-center"
+            className="w-[9rem] h-[9rem] hover:scale-110 hover:cursor-pointer transition-all ease-out bg-white flex-col flex rounded-full justify-center items-center"
           >
-            <TechIcon.logo size={42} />
+            <TechIcon.logo size={48} />
             {TechIcon.name}
           </div>
         ))}
